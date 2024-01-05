e1b6a645d06359aca9a9d6a8ab16a7b4b1c3007a
Jung Woo Lee <phyjics@gmail.com>
2024-01-05 12:02:38 +0900

    LTHelixTrackFindingTask added to run_eve

--- macros/run_eve.C
+++ macros/run_eve.C
@@ -5,16 +5,22 @@ void next(int eventID=-1) {
         LKRun::GetRun()->ExecuteNextEvent();
 }
 
+#include "LKLogger.h"
+
 void run_eve()
 {
+    lk_logger("log/eve.log");
     auto run = new LKRun();
     run -> SetTag("eve");
     run -> AddPar("config_lamps.mac");
     run -> AddInputFile("data/lamps_0000.0.all.root");
     run -> Add(new LKPulseShapeAnalysisTask);
+    run -> Add(new LTHelixTrackFindingTask);
     run -> Add(new LKEveTask);
     run -> AddDetector(new LAMPSTPC());
     run -> Init();
 
     next();
+    //next(8);
+    //next(39);
 }
